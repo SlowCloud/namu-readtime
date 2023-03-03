ea12dcc4707964f27e9b8611bf53dfd3f5056d85
slowcloud <sju9417@gmail.com>
2023-03-03 12:29:05 +0900

    fixed bug

--- scripts/badge.js
+++ scripts/badge.js
@@ -1,18 +1,19 @@
 var badge;
 function removeBadge() {
-    if(badge) badge.remove();
+  if (badge) badge.remove();
 }
 
 function buildBadge() {
-    badge = document.createElement("p");
-    const article = document.getElementsByClassName("rnvX2WIO")[0];
+  badge = document.createElement("p");
+  const article = document.getElementsByClassName("rnvX2WIO")[0];
+  console.log(article);
 
-    const text = article.textContent;
-    const reg = /[^\s]+/g;
-    const words = text.matchAll(reg);
-    const readingTime = Math.round([...words].length / 200);
-    badge.textContent = `⏱️ 해당 항목을 읽는 데 약 ${readingTime}분이 걸립니다.`;
+  const text = article.textContent;
+  const reg = /[^\s]+/g;
+  const words = text.matchAll(reg);
+  const readingTime = Math.round([...words].length / 200);
+  badge.textContent = `⏱️ 해당 항목을 읽는 데 약 ${readingTime}분이 걸립니다.`;
 
-    const title = article.querySelector("h1");
-    title.insertAdjacentElement("beforebegin", badge);
+  const title = article.querySelector("h1");
+  title.insertAdjacentElement("beforebegin", badge);
 }
